9e5171df3fa87c661a7c36a53f35b31760999895
Mario Carretero Berrocal <mariocb@gamma16.aulas.gsyc.urjc.es>
2016-11-21 15:47:15 +0100

    Terminada

--- client.py
+++ client.py
@@ -23,12 +23,13 @@ except IndexError:
     sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 
 # Contenido que vamos a enviar
-LINE = metodo + ' sip:' + datos.split(':')[0] + ' SIP/2.0 \r\n\r\n'
+LINE = metodo + ' sip:' + datos.split(':')[0] + ' SIP/2.0\r\n'
+print(ip)
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, int(PORT)))
+my_socket.connect((ip, int(PORT)))
 
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
@@ -40,11 +41,10 @@ response = data.decode('utf-8')
 slices =  response.split()
 if slices[1] == '100' and slices[4] == '180' and slices[7] == '200':
     method = 'ACK'
-    AutoLINE = method + ' sip:' + datos.split(':')[0] + ' SIP/2.0 \r\n\r\n'
+    AutoLINE = method + ' sip:' + datos.split(':')[0] + ' SIP/2.0\r\n\r\n'
     print('Enviando: ' + AutoLINE)
     my_socket.send(bytes(AutoLINE, 'utf-8') + b'\r\n\r\n')
 
-print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
 
 # Cerramos todo
--- server.py
+++ server.py
@@ -23,35 +23,42 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        ip = self.client_address[0]
-        PORT = self.client_address[1]
+        #ip = self.client_address[0]
+        #PORT = self.client_address[1]
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read().decode('utf-8')
+            line = self.rfile.read()
+            line = line.decode('utf-8')
+            print("El cliente nos manda " + line)
             line_slices = line.split()
+            
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
             metodo = line_slices[0]
             if metodo == 'INVITE':
-                self.wfile.write(b'SIP/2.0 100 Trying \r\n\r\n')
-                self.wfile.write(b'SIP/2.0 180 Ring \r\n\r\n')
-                self.wfile.write(b'SIP/2.0 200 OK \r\n\r\n')
+                self.wfile.write(b'SIP/2.0 100 Trying\r\n\r\n')
+                self.wfile.write(b'SIP/2.0 180 Ring\r\n\r\n')
+                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
             elif metodo == 'BYE':
-                self.wfile.write(b'SIP/2.0 200 OK \r\n\r\n')
+                self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
             elif metodo == 'ACK':
-                aEjecutar = 'mp32rtp -i ' + ip + '-p 23032 < ' + audio_file
+                aEjecutar = 'mp32rtp -i ' + IP + ' -p 23032 < ' + audio_file
                 print ('Vamos a ejecutar', aEjecutar)
                 os.system(aEjecutar)
+                print('Finished transfer')
             elif metodo != 'INVITE' or metodo != 'BYE' or metodo != 'ACK':
-                self.wfile.write(b'SIP/2.0 405 Method Not Allowed \r\n\r\n')
+                self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n\r\n')
             else:
                 self.wfile.write(b'SIP/2.0 400 Bad Request')
-            #print("El cliente nos manda " + line.decode('utf-8'))
+        
 
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', int(PORT)), EchoHandler)
-    print("Listening...")
-    serv.serve_forever()
+    if os.path.exists(audio_file):
+        serv = socketserver.UDPServer((IP, int(PORT)), EchoHandler)
+        print("Listening...")
+        serv.serve_forever()
+    else:
+        sys.exit('Audio file not found')
